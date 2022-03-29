be05a691d3c10cb8a893bcc3dc238a747eef5e90
drsimplegraffiti <abayomiogunnusi@gmail.com>
2022-03-29 17:32:57 +0100

    added crud operation v1

--- blog/main.py
+++ blog/main.py
@@ -29,14 +29,13 @@ async def create(request: schemas.Blog, db: Session = Depends(get_db)):
 
 # Update a blog by id
 @app.put("/blog/{id}", status_code=status.HTTP_202_ACCEPTED)
-async def update(id: int, request: schemas.Blog, db: Session = Depends(get_db)):
-    blog = db.query(models.Blog).filter(models.Blog.id == id).update(request)
-    if not blog:
+async def update(id, request: schemas.Blog, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+    if not blog.first():
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Blog with id {id} not found")
-    blog.title = request.title
-    blog.body = request.body
+    blog.update(request)
     db.commit()
-    return blog
+    return "updated"
 
 # Get all blogs
 @app.get("/blogs")
@@ -56,11 +55,13 @@ async def read_one(id: int, db: Session = Depends(get_db)):
 # Delete a blog by id
 @app.delete("/blog/{id}", status_code=status.HTTP_204_NO_CONTENT)
 async def delete(id: int, db: Session = Depends(get_db)):
-    blog = db.query(models.Blog).filter(models.Blog.id == id).delete(synchronize_session=False)
-    db.commit()
-    if not blog:
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+    if not blog.first():
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Blog with id {id} not found")
-    return "done"
+    blog.delete(synchronize_session=False)
+    db.commit()
+    return "deleted"
 
 
 
