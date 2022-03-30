b4d0ed737b573ae418eb11d71a56ffcc67f37841
drsimplegraffiti <abayomiogunnusi@gmail.com>
2022-03-30 20:26:32 +0100

    complete crud and auth

--- blog/routers/blog.py
+++ blog/routers/blog.py
@@ -1,7 +1,7 @@
 from typing import List
 from fastapi import APIRouter, status, FastAPI, Depends, HTTPException, status, Response
 from sqlalchemy.orm import Session
-from .. import schemas, database, models
+from .. import schemas, database, models, oauth2
 from ..repository import blog
 
 router = APIRouter(
@@ -17,31 +17,31 @@ async def root():
 
 # Get all blogs
 @router.get("/", status_code=status.HTTP_200_OK,response_model=List[schemas.ShowBlog])
-async def read_all(db: Session = Depends(get_db)):
+async def read_all(db: Session = Depends(get_db),current_user: schemas.User = Depends(oauth2.get_current_user) ):
     return blog.get_all(db)
     
 
 
 # Create a blog
 @router.post("/", status_code=status.HTTP_201_CREATED)
-async def create(request: schemas.Blog, db: Session = Depends(get_db)):
+async def create(request: schemas.Blog, db: Session = Depends(get_db),current_user: schemas.User = Depends(oauth2.get_current_user)):
     return blog.create(request, db)
 
 
 
 # Update a blog by id
 @router.put("/{id}", status_code=status.HTTP_202_ACCEPTED)
-async def update(id:int, request: schemas.Blog, db: Session = Depends(get_db)):
+async def update(id:int, request: schemas.Blog, db: Session = Depends(get_db),current_user: schemas.User = Depends(oauth2.get_current_user)):
     return blog.update(id, request, db)
 
 
 # Get a blog by id
 @router.get("/{id}", status_code=status.HTTP_200_OK, response_model=schemas.ShowBlog)
-async def read_one(id: int, db: Session = Depends(get_db)):
+async def read_one(id: int, db: Session = Depends(get_db),current_user: schemas.User = Depends(oauth2.get_current_user)):
     return blog.show(id,db)
 
 
 # Delete a blog by id
 @router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
-async def delete(id: int, db: Session = Depends(get_db)):
+async def delete(id: int, db: Session = Depends(get_db),current_user: schemas.User = Depends(oauth2.get_current_user)):
     return blog.destroy(id, db)
--- blog/routers/user.py
+++ blog/routers/user.py
@@ -2,8 +2,7 @@ from typing import List
 from fastapi import APIRouter, status, FastAPI, Depends, HTTPException, status, Response
 from sqlalchemy.orm import Session
 from .. import schemas, database, models
-from ..hashing import Hash
-
+from ..repository import user
 
 router = APIRouter(
      prefix ="/user",
@@ -14,19 +13,11 @@ get_db= database.get_db
 
 @router.post("/", status_code=status.HTTP_201_CREATED, response_model = schemas.ShowUser)
 async def create_user(request: schemas.User, db:Session = Depends(get_db)):
-    new_user = models.User(name=request.name, email=request.email, password=Hash.bcrypt(request.password))
-    db.add(new_user)
-    db.commit()
-    db.refresh(new_user) 
-    return new_user
-
+    return user.create(request,db)
 
 
 # Get user information
 @router.get("/{id}", status_code=status.HTTP_201_CREATED, response_model = schemas.ShowUser)
 async def get_user(id: int, db:Session = Depends(get_db)):
-    user = db.query(models.User).filter(models.User.id == id).first()
-    if not user:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"user with id {id} not found")
-    return user
+    return user.show(id,db)
 
